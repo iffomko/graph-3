4aa56c22cf8280dd6e56471b34979609ef7073fb
Cromulent74 <ilya.fomko03@mail.ru>
2022-05-10 16:16:41 +0500

    read graph in main file

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+#include <fstream>
+
+#include "readGraph/readGraph.h"
+#include "list/list.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::ifstream fin("C:\\Users\\Iluha\\Documents\\github_repos\\graph-3\\input.txt");
+    int n;
+
+    list* graph = readGraph(fin, n);
+
+    for (int i = 0; i < n; i++) {
+        graph[i].print();
+        std::cout << "\n";
+    }
+
     return 0;
 }
